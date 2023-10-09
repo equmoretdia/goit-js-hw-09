af7e17f0ddccf6955a0f17ba50f2bf0e699643fe
Serhiy Belinskyy <134813907+equmoretdia@users.noreply.github.com>
2023-10-09 16:50:17 +0200

    task 1 is completed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
-const a = null; //to change
+const body = document.querySelector('body');
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+
+let timerId = null;
+
+stopButton.disabled = true;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function changeBackgroundColor() {
+  body.style.background = getRandomHexColor();
+}
+
+function handleStartClick() {
+  timerId = setInterval(changeBackgroundColor, 1000);
+  //   console.log(timerId);
+  startButton.disabled = true;
+  stopButton.disabled = false;
+  return timerId;
+}
+
+function handleStopClick() {
+  clearInterval(timerId);
+  //   console.log(timerId);
+  startButton.disabled = false;
+  stopButton.disabled = true;
+}
+
+startButton.addEventListener('click', handleStartClick);
+stopButton.addEventListener('click', handleStopClick);
